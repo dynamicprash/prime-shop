2aa3111e3c6088e45a9f24243a627bddd79e8efb
Prashant Paudel <prashantpaudel32109@gmail.com>
2025-11-10 14:45:04 +0545

    Add the middlewares and utils

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,14 +7,19 @@ dotenv.config({path: './env'});
 
 
 const app = express();
-connectDb();
+
+connectDb()
+.then(() => {
+    console.log("MongoDB connected successfully");
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server running at http://localhost:${process.env.PORT || 3000}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed",err);
+    process.exit(1);
+})
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 })
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    }
-);
